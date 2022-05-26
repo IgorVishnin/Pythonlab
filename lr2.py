c01ca781041188b3b8ded3011d68f66594568afe
IgorVishnin <99646567+IgorVishnin@users.noreply.github.com>
2022-05-26 13:45:40 +0300

    Update lr2.py

--- lr2.py
+++ lr2.py
@@ -1,16 +1,16 @@
-строка  =  строка ( ввод ( "Введите строку: " ))
+string = str(input("Enter string: "))
 
-print ( "Строка без третьего символа: " , end  =  '' )
-для  этого char  в enumerate  ( string ) :
-    если ( это  ==  2 ):
-        Продолжать
-    печать ( символ , конец  =  '' )
-распечатать ()
+print("String without 3rd symbol: ", end = '')
+for it, char in enumerate(string):
+    if (it == 2):
+        continue
+    print(char, end = '')
+print()
 
-если ( строка . найти ( 'c' ) !=  - 1 ):
-    print ( "Эта строка содержит символ \" c \" . )
-еще :
-    print ( "Эта строка не имеет собственного символа \" c \" . )
+if (string.find('c') != -1):
+    print("This string owns symbol \"c\".")
+else:
+    print("This string doesn't own symbol \"c\".")
 
-print ( "Длина строки: {}" .format ( len ( string ) ))
-print ( "Строка без последнего символа: {}" . format ( строка [: - 1 :]))
+print("String length: {}".format(len(string)))
+print("String without last symbol: {}".format(string[:-1:]))
